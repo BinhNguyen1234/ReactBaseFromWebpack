ac34bd6cd3b1a3240b00fdf1f35a6685f9c73ba5
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-11-25 15:08:55 +0700

    init:

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import ReactDOM from 'react-dom/client'
+import FFFF from './FFF';
+ReactDOM.createRoot(document.getElementById('root')).render(
+        <React.StrictMode>
+            <Fff></Fff>
+        </React.StrictMode>
+)
+function Fff(){
+    return (
+        <>
+            <div>414124124</div>
+            <FFFF></FFFF>
+        </>
+    )
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import {CleanWebpackPlugin} from 'clean-webpack-plugin'
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import webpack from "webpack";
+import Dotenv from "dotenv-webpack";
+import TerserPlugin from "terser-webpack-plugin";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(process.env);
 const config = {
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
-  devtool: "eval-source-map",
-  plugins: [ 
+  devtool: "source-map",
+  plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname,'index.html'),
-      filename: 'index.html'
+      template: path.resolve(__dirname, "index.html"),
+      filename: "index.html",
     }),
-    new CleanWebpackPlugin() 
+    new Dotenv(),
+    new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin()
   ],
   loader: {},
   module: {
@@ -27,8 +35,24 @@ const config = {
       },
     ],
   },
+  optimization: {
+    nodeEnv: "production",
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            dead_code: true,
+            // drop_console: true,
+            drop_debugger: true,
+          },
+        },
+      }),
+    ],
+  },
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
 };
+
 export default config;
