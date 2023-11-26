3f35505c38b8d6d1310432330437548d16008686
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-11-26 21:57:01 +0700

    config code spliting and http-serve for test

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,19 +1,37 @@
 import { createBrowserRouter } from 'react-router-dom'
-import HomePage from './Home'
-import DefaultPage from './Default'
+// import NavBar from '../layout/LayoutCommon'
+import { type ComponentType, Suspense, lazy } from 'react'
 import NavBar from '../layout/LayoutCommon'
+
+const HomePage = lazy(async () => {
+    return await import('./Home')
+})
+/**
+ * this is mockup incase lazy load take 3s to import component, in real life, there none coding like this
+ */
+const DefaultPage = lazy(async () => {
+    const c = new Promise((rs, rj) => {
+        setTimeout(() => {
+            rs(import('./Default'))
+        }, 3000)
+    })
+    const a = c.then(rs => {
+        return rs
+    })
+    return await (a as Promise<{ default: ComponentType<any> }>)
+})
 const router = createBrowserRouter([
     {
-        element: <NavBar/>,
+        element: <NavBar />,
         path: '/',
         children: [
             {
                 path: '/home',
-                element: <HomePage/>
+                element: <Suspense fallback={<div>Loading</div>} ><HomePage /></Suspense>
             },
             {
                 path: '',
-                element: <DefaultPage/>
+                element: <Suspense fallback={<div>Loading</div>} ><DefaultPage /></Suspense>
             }
         ]
     }
