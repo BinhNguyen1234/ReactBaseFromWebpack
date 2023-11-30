cba595bf9c2e722abc0f929451f42ab4ee444526
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-11-30 23:20:16 +0700

    ok

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -2,7 +2,7 @@ import { merge } from 'webpack-merge'
 import configCommon from './webpack.common.config.js'
 const configLocal = {
     mode: 'development',
-    devtool: 'cheap-source-map'
+    devtool: 'eval-source-map'
 }
 const config = merge(configCommon, configLocal)
 
